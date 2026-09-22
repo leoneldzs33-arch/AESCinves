a45edb05c9f45052e14c8aa0528c276113632ac6
Lest <leoneldzs33@gmail.com>
2026-09-21 22:59:24 -0600

    Primer intento mixculumns

--- mixcolumns/mixcolumns.c
+++ mixcolumns/mixcolumns.c
@@ -17,23 +17,26 @@ unsigned char colu1 [4]= //columna 1
 
 unsigned char primer_columna= estado[1,5,9,13]//primer columna
 //sigue una operacion vector * matriz
-result = //operacion v * m
+result2 = //operacion v * m
 unsigned char colu2 [4]= //columna 2
 
 unsigned char primer_columna= estado[2,6,10,14]//primer columna
 //sigue una operacion vector * matriz
-result = //operacion v * m
+result3 = //operacion v * m
 unsigned char colu1 [4]= //columna 3
 
 unsigned char primer_columna= estado[3,7,11,15]//primer columna
 //sigue una operacion vector * matriz
-result = //operacion v * m
+result4 = //operacion v * m
 unsigned char colu1 [4]= //columna 4
 //=
 unsigned char matriz_mix [4][4] = //resultado de la union de las 4 columnas resultantes
 
 
-
+mensaje_alex= creo que la estructura es correcta , 
+solo me falta ver como hacer en C vector * matriz y saber como uno la reduccion con esto 
+, los 8 bits resultantes del acomodo que son ? la matriz de estado que declaro al inicio, 
+la proveniente de shiftrows --> acomodo-->matriz de estado inicial de mixcolunms?"
 
 
 }

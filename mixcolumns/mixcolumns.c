5fbc9ca7434fb981e9a79cf96802d8090780e65b
Lest <leoneldzs33@gmail.com>
2026-09-21 22:56:23 -0600

    Primer intento mixculumns

---
+++ mixcolumns/mixcolumns.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+unsigned char estado [4][4] = //matriz proveniente de shiftrows 
+unsigned char matriz_const = {
+{02,03,01,01},
+{01,02,03,01},
+{01,01,02,03},
+{03,01,01,02}
+};
+unsigned char primer_columna= estado[0,4,8,12]//primer columna
+//sigue una operacion vector * matriz
+result = //operacion v * m
+unsigned char colu1 [4]= //columna 1
+
+
+unsigned char primer_columna= estado[1,5,9,13]//primer columna
+//sigue una operacion vector * matriz
+result = //operacion v * m
+unsigned char colu2 [4]= //columna 2
+
+unsigned char primer_columna= estado[2,6,10,14]//primer columna
+//sigue una operacion vector * matriz
+result = //operacion v * m
+unsigned char colu1 [4]= //columna 3
+
+unsigned char primer_columna= estado[3,7,11,15]//primer columna
+//sigue una operacion vector * matriz
+result = //operacion v * m
+unsigned char colu1 [4]= //columna 4
+//=
+unsigned char matriz_mix [4][4] = //resultado de la union de las 4 columnas resultantes
+
+
+
+
+
+}
